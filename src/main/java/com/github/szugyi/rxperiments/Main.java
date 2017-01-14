3e9dc37201dbe24a0b8d5aee2ff089212fda0285
Csaba Szugyiczki <szugyiczkicsaba@gmail.com>
2017-01-14 20:39:11 +0100

    Create initial project

---
+++ src/main/java/com/github/szugyi/rxperiments/Main.java
@@ -0,0 +1,12 @@
+package com.github.szugyi.rxperiments;
+
+import java.io.IOException;
+
+/**
+ * Created by szugyi on 14/01/17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
